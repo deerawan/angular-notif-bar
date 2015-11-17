b022db8b629e21c47a9bd7efb67e79d8e0a83c20
Budi Irawan <deerawan@gmail.com>
2015-11-17 20:53:37 +0700

    configure uglify and less in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,20 @@
 
 var gulp = require('gulp');
 var less = require('gulp-less');
-var path = require('path');
+var uglify = require('gulp-uglify');
 
 gulp.task('less', function() {
   return gulp.src('./src/**/*.less')
-    .pipe(less);
-    .pipe(gulp.dest('./dest'));
+    .pipe(less())
+    .pipe(gulp.dest('./dist'));
 });
+
+gulp.task('uglify', function() {
+  return gulp.src('src/*.js')
+    .pipe(uglify({
+      outSourceMap: true
+    }))
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('default', ['less', 'uglify']);
