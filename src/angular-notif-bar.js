58652ab8a189d9bf5fb6999f3f581de91f8ade76
Budi Irawan <deerawan@gmail.com>
2015-11-17 20:01:06 +0700

    add LESS

--- src/angular-notif-bar.js
+++ src/angular-notif-bar.js
@@ -7,29 +7,39 @@
 
     function notifBarProvider() {
 
-      this.options = {
+      var vm = this;
+
+      vm.options = {
         delay: 5000
       };
 
-      this.setOptions = function(options) {
-        if (!angular.isObject(options)) throw new Error('Options format should be an object');
-        this.options = angular.extend({}, this.options, options);
+      vm.setOptions = function(options) {
+        if (!angular.isObject(options)) {
+          throw new Error('Options format should be an object');
+        }
+        vm.options = angular.extend({}, vm.options, options);
       };
 
-      this.$get = notifBar;
+      vm.$get = notifBar;
 
-      function notifBar() {
+      function notifBar($http, $templateCache, $compile, $rootScope) {
         var service = {
-
+          show: show
         };
 
         return service;
 
-        var options = this.options;
+        var options = vm.options;
 
-        $http.get(args.template, {cache: $templateCache}).success(function(template) {
+        function show(args, type) {
 
-        });
+          var scope = $rootScope.$new();
+
+          $http.get(args.template, {cache: $templateCache}).success(function(template) {
+            var templateElement = $compile(template)(scope);
+            angular.element(document.getElementsByTagName('body')).append(templateElement);
+          });
+        }
       }
     }
 
